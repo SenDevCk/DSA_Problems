d8892b4b7249aa3a0b4d506c506e53e156806723
chandan101288@gmail.com <chandan101288@gmail.com>
2025-08-07 11:29:22 +0530

    problem serching in rotated array

---
+++ Problems/src/com/example/Problem11.java
@@ -0,0 +1,11 @@
+package com.example;
+
+//search rotated array
+public class Problem11 {
+   
+	public int searchInRotatedArray(int[] arr,int targer) {
+		int result=0;
+		
+		return result;
+	}
+}
